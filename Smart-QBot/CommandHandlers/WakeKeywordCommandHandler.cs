3934d3e0d5fa7700cfbe9c142fe5e5aa69a85738
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-18 00:10:40 +0300

    WakeKeywordCommandHandler.cs

---
+++ Smart-QBot/CommandHandlers/WakeKeywordCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using PerryQBot.Commands;
+using Volo.Abp.DependencyInjection;
+
+namespace PerryQBot.CommandHandlers;
+
+[Command("ai", false)]
+[Command("喵", false)]
+[Command("呐", false)]
+[ExposeServices(typeof(ICommandHandler))]
+public class WakeKeywordCommandHandler : CommandHandlerBase
+{
+    public override bool IsContinueAfterHandled => true;
+}
