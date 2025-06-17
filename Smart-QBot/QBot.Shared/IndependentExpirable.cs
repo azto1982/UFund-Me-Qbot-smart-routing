43ad24b6b5460dd9ef10bf8e3f9d2dcdc21356aa
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-18 00:12:33 +0300

    IndependentExpirable.cs

---
+++ Smart-QBot/QBot.Shared/IndependentExpirable.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics;
+
+namespace SaladimQBot.Shared;
+
+[DebuggerDisplay("{(!hasValueCache ? \"[NoValueCached]\" : \"\")} {valueCache}")]
+public class IndependentExpirable<T> : IIndependentExpirable<T>, IExpirable<T>
+{
+    protected Func<T> valueFactory;
+    protected T? valueCache = default;
+    protected bool hasValueCache = false;
+    protected TimeSpan expireTimeSpan;
+    protected DateTime expireDateTime;
+
+    public TimeSpan ExpireTimeSpan => expireTimeSpan;
+
+    public bool IsExpired => !hasValueCache || DateTime.Now > expireDateTime;
+
+    public T Value
+    {
+        get
+        {
+            if (!IsExpired)
+                return valueCache!;
+            lock (valueFactory)
+            {
+                if (!IsExpired)
+                    return valueCache!;
+                T newValue = valueFactory();
+                expireDateTime = DateTime.Now + expireTimeSpan;
+                valueCache = newValue;
+                hasValueCache = true;
+                return newValue;
+            }
+        }
+    }
+
+    public IndependentExpirable(Func<T> valueFactory, TimeSpan expireTimeSpan)
+    {
+        this.valueFactory = valueFactory;
+        this.expireTimeSpan = expireTimeSpan;
+    }
+
+    public IndependentExpirable(T presetValue, Func<T> valueFactory, TimeSpan expireTimeSpan)
+    {
+        this.hasValueCache = true;
+        this.valueCache = presetValue;
+        this.valueFactory = valueFactory;
+        this.expireTimeSpan = expireTimeSpan;
+        this.expireDateTime = DateTime.Now + expireTimeSpan;
+    }
+}
+
+public interface IIndependentExpirable<out T> : IExpirable<T>
+{
+
+}
