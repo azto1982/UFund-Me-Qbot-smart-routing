542655e91827a73b11cbf5fbb03e802e4f39d1b5
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-18 00:10:33 +0300

    PresetSetCommandHandler.cs

---
+++ Smart-QBot/CommandHandlers/PresetSetCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PerryQBot.Commands;
+using PerryQBot.EntityFrameworkCore.Entities;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Repositories;
+
+namespace PerryQBot.CommandHandlers;
+
+[Command("preset")]
+[Command("预设")]
+[ExposeServices(typeof(ICommandHandler))]
+public class PresetSetCommandHandler : CommandHandlerBase
+{
+    public IRepository<User> UserRepository { get; set; }
+
+    public override async Task ExecuteAsync(CommandContext context)
+    {
+        var (isCommand, commandString, messageString) = this.TryGetCommand(context.Message);
+        var user = await (await UserRepository.WithDetailsAsync(x => x.History)).FirstOrDefaultAsync(t => t.QQ == context.SenderId);
+
+        if (user is not null)
+        {
+            var history = user.History.OrderByDescending(x => x.Id).Take(BotOptions.Value.MaxHistory).ToList();
+            await UserRepository.UpdateAsync(user, true);
+        }
+        if (user is null)
+        {
+            await UserRepository.InsertAsync(new User
+            {
+                QQ = context.SenderId,
+                QQNickName = context.SenderName,
+                History = new List<UserHistory>(),
+                Preset = messageString
+            }, true);
+        }
+        else
+        {
+            user.Preset = messageString;
+            user.History.Clear();
+            await UserRepository.UpdateAsync(user, true);
+        }
+
+        ResponseMessage = $"预设修改成功";
+    }
+}
