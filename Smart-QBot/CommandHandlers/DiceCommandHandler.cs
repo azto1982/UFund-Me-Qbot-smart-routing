b13e53f7f519c57bd28836670b3ce40d2bd00740
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-18 00:10:23 +0300

    DiceCommandHandler.cs

---
+++ Smart-QBot/CommandHandlers/DiceCommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using Mirai.Net.Utils.Scaffolds;
+using PerryQBot.Commands;
+using Volo.Abp.DependencyInjection;
+
+namespace PerryQBot.CommandHandlers;
+
+[Command("骰子")]
+[ExposeServices(typeof(ICommandHandler))]
+public class DiceCommandHandler : CommandHandlerBase
+{
+    public override MessageChainBuilder OnMessageChainBuilding(MessageChainBuilder builder)
+    {
+        var number = new Random(DateTime.Now.Millisecond).Next(1, 7);
+        return builder.Dice(number.ToString());
+    }
+}
