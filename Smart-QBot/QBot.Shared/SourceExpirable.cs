e0be951abe38d03b14566415f7042432a94bd73e
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-18 00:12:38 +0300

    SourceExpirable.cs

---
+++ Smart-QBot/QBot.Shared/SourceExpirable.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace SaladimQBot.Shared;
+
+[DebuggerDisplay("IndepenExp -> {independentExpirable}")]
+public class SourceExpirable<T> : IDependencyExpirable<T>
+{
+    protected IndependentExpirable<T> independentExpirable;
+
+    public IDependencyExpirable<object> Upstream => (IDependencyExpirable<object>)this;
+
+    public T Value => independentExpirable.Value;
+
+    public bool IsExpired => independentExpirable.IsExpired;
+
+    public SourceExpirable(IndependentExpirable<T> independentExpirable)
+    {
+        this.independentExpirable = independentExpirable;
+    }
+}
