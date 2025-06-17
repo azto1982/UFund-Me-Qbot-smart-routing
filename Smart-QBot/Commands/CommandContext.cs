874eba903dd57930c1b9b6b60b0ee527e6c1b617
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-18 00:10:46 +0300

    CommandContext.cs

---
+++ Smart-QBot/Commands/CommandContext.cs
@@ -0,0 +1,13 @@
+﻿using Mirai.Net.Data.Messages;
+
+public class CommandContext
+{
+    public MessageReceivers Type { get; set; }
+    public string SenderId { get; set; }
+    public string SenderName { get; set; }
+    public string GroupId { get; set; }
+    public string GroupName { get; set; }
+    public string Message { get; set; }
+    public string CommandString { get; set; }
+    public MessageChain MessageChain { get; set; }
+}
