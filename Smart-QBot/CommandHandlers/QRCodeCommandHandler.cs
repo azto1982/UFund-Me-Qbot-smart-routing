3f57debb84df8b262a7e6e70f08b62225efe587f
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-18 00:10:35 +0300

    QRCodeCommandHandler.cs

---
+++ Smart-QBot/CommandHandlers/QRCodeCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Options;
+using Mirai.Net.Utils.Scaffolds;
+using PerryQBot.Commands;
+using PerryQBot.Options;
+using Volo.Abp.DependencyInjection;
+
+namespace PerryQBot.CommandHandlers;
+
+[Command("qrcode")]
+[Command("二维码")]
+[ExposeServices(typeof(ICommandHandler))]
+public class QRCodeCommandHandler : CommandHandlerBase
+{
+    public IOptions<Apis> Apis { get; set; }
+    public string ImageUrl { get; set; }
+
+    public override async Task ExecuteAsync(CommandContext context)
+    {
+        ImageUrl = $"{Apis.Value.UomgAPI}/qrcode?url={context.Message.Trim()}";
+        await Task.CompletedTask;
+    }
+
+    public override MessageChainBuilder OnMessageChainBuilding(MessageChainBuilder builder) => builder.ImageFromUrl(ImageUrl);
+}
