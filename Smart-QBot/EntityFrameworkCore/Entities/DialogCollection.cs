9d2e7246692c6d3ffedbddf93358fba5b105f1df
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-18 00:11:01 +0300

    DialogCollection.cs

---
+++ Smart-QBot/EntityFrameworkCore/Entities/DialogCollection.cs
@@ -0,0 +1,12 @@
+﻿using Volo.Abp.Domain.Entities;
+
+namespace PerryQBot.EntityFrameworkCore.Entities;
+
+public class DialogCollection : Entity<long>
+{
+    public string UserName { get; set; }
+    public string UserQQ { get; set; }
+    public string Message { get; set; }
+    public string QuoteMessage { get; set; }
+    public DateTime DateTime { get; set; }
+}
