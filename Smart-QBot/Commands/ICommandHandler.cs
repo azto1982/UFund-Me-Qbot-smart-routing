e78b67c6b13fdadb071e36bd95fdf5f94bd8d0cc
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-18 00:10:51 +0300

    ICommandHandler.cs

---
+++ Smart-QBot/Commands/ICommandHandler.cs
@@ -0,0 +1,10 @@
+﻿namespace PerryQBot.Commands;
+
+public interface ICommandHandler
+{
+    bool IsContinueAfterHandled { get; set; }
+    string ResponseMessage { get; set; }
+    string RequestMessage { get; set; }
+
+    Task HandleAsync(CommandContext context);
+}
