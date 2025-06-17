3fd64bdf0798d5502bdfe6fc65e62d65313b6c73
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-18 00:10:44 +0300

    CommandAttribute.cs

---
+++ Smart-QBot/Commands/CommandAttribute.cs
@@ -0,0 +1,16 @@
+﻿[AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
+public class CommandAttribute : Attribute
+{
+    public CommandAttribute(string command, bool withPrefix = true)
+    {
+        Command = command;
+        WithPrefix = withPrefix;
+    }
+
+    public CommandAttribute()
+    { }
+
+    public bool WithPrefix { get; set; } = true;
+    public string Command { get; set; }
+    public bool WorkerCanSchedule { get; set; } = false;
+}
