5a5c785ac90c7b3e485e28b24a1d64599a7f1e9b
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-18 00:12:23 +0300

    MiraiBotOptions.cs

---
+++ Smart-QBot/Options/MiraiBotOptions.cs
@@ -0,0 +1,10 @@
+﻿public class MiraiBotOptions
+{
+    public string Host { get; set; }
+    public int Port { get; set; }
+    public string QQ { get; set; }
+    public string AdminQQ { get; set; }
+    public string VerifyKey { get; set; }
+    public int MaxHistory { get; set; }
+    public string CommandPrefix { get; set; } = "$$";
+}
