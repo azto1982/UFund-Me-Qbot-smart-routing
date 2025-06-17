753df874b8aa84d05b16d8ffc83b8814f6b2278e
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-18 00:10:12 +0300

    QBotModule.cs

---
+++ Smart-QBot/QBotModule.cs
@@ -0,0 +1,32 @@
+﻿using PerryQBot.EntityFrameworkCore;
+using PerryQBot.Options;
+using PerryQBot.QQBot;
+using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.Autofac;
+using Volo.Abp.Modularity;
+using Volo.Abp.Uow;
+
+namespace PerryQBot
+{
+    [DependsOn(
+        typeof(AbpAspNetCoreMvcModule),
+        typeof(AbpUnitOfWorkModule),
+        typeof(AbpAutofacModule)
+    )]
+    [DependsOn(
+        typeof(QBotOpenAIModule),
+        typeof(QBotEntityFrameworkModule),
+        typeof(QBotMiraiQQModule)
+    )]
+    public class QBotModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            var configuration = context.Services.GetConfiguration();
+            Configure<MessageCollectionOptions>(configuration.GetSection(nameof(MessageCollectionOptions)));
+            Configure<Apis>(configuration.GetSection(nameof(Apis)));
+
+            context.Services.AddDistributedMemoryCache();
+        }
+    }
+}
