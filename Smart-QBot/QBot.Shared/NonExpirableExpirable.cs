2ee15ca86dbff512903c2ee08094e39204b48403
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-18 00:12:35 +0300

    NonExpirableExpirable.cs

---
+++ Smart-QBot/QBot.Shared/NonExpirableExpirable.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+
+namespace SaladimQBot.Shared;
+
+[DebuggerDisplay("[NonExpirable] {Value}")]
+public class NonExpirableExpirable<T> : IExpirable<T>
+{
+    protected Lazy<T> lazyValue;
+
+    public T Value => lazyValue.Value;
+
+    public bool IsExpired => false;
+
+    public NonExpirableExpirable(T initValue)
+    {
+#if NETSTANDARD2_0
+        lazyValue = new(() => initValue, true);
+#else
+        lazyValue = new(initValue);
+#endif
+    }
+
+    public NonExpirableExpirable(Func<T> valueFactory)
+    {
+        lazyValue = new(valueFactory, true);
+    }
+}
