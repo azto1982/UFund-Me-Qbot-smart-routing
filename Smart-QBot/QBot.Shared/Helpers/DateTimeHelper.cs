0a9b64c18af3a02a143a2f2a01c732ab9315e02d
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-18 00:12:40 +0300

    DateTimeHelper.cs

---
+++ Smart-QBot/QBot.Shared/Helpers/DateTimeHelper.cs
@@ -0,0 +1,15 @@
+﻿namespace SaladimQBot.Shared;
+
+public static class DateTimeHelper
+{
+    public static DateTime GetFromUnix(long unixTime)
+        => new DateTime(1970, 1, 1).AddSeconds(unixTime).ToLocalTime();
+
+    public static DateTime GetFromUnix(uint unixTime)
+        => GetFromUnix((long)unixTime);
+
+    public static long ToUnix(DateTime dateTime)
+    {
+        return (dateTime.ToUniversalTime().Ticks - 621355968000000000) / 10000000;
+    }
+}
